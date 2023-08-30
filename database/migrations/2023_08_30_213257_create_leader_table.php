30b265d3a37757c967f22eba63f1bee4c3252e30
Shashika Mahindapala <shashika@mdidev.com>
2023-08-30 19:47:10 -0300

    added events

---
+++ database/migrations/2023_08_30_213257_create_leader_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leaders', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->unsignedTinyInteger('age');
+            $table->smallInteger('points');
+            $table->string('address', 255);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('leader');
+    }
+};
